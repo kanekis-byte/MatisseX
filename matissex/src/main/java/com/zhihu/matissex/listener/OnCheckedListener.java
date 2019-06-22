a788dea249c32f16dad6478e940a4c02f23d1488
Mr.Taohf <hongfei.engineer@gmail.com>
2019-06-22 20:34:20 +0800

    [初始化项目：本地拉取matisse的源码进行迁移到androidX]

---
+++ matissex/src/main/java/com/zhihu/matissex/listener/OnCheckedListener.java
@@ -0,0 +1,9 @@
+package com.zhihu.matissex.listener;
+
+
+/**
+ *  when original is enabled , callback immediately when user check or uncheck original.
+ */
+public interface OnCheckedListener {
+    void onCheck(boolean isChecked);
+}
